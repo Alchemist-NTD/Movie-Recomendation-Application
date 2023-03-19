0c7e7cd3c51c5ebf5bc6b80506e6805253e2a934
Duc Do <doduc9569@gmail.com>
2023-03-19 14:34:27 +0700

    feature0 is done

--- Frontend/movie_webapp/src/App.js
+++ Frontend/movie_webapp/src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { SearchBar } from './search_bar';
+import { Login } from './login.jsx';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="bg-auto md:bg-contain">
+        
+      </div>
+      <SearchBar>
+        SearchBar
+      </SearchBar>
+
+      <Login>Hello</Login>
+      
     </div>
   );
 }
---
+++ Frontend/movie_webapp/src/search_bar.jsx
@@ -0,0 +1,18 @@
+
+import './search.css'
+export const SearchBar = () => {
+    return (
+        
+        <div class='widget-search'>
+            
+            <form action="get">
+            <input type='text' name='search' placeholder='seatch other' id='search-keyword'>
+
+            </input>
+            <input id='searchsubmit' type='submit'>
+            </input>
+            </form>
+            
+        </div>
+    )
+}
